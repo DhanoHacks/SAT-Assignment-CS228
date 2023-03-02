4ec085886e8abc4f9d77c98fc71bd07528eb822f
DhanoHacks <draman9224@gmail.com>
2023-03-02 15:55:24 +0530

    Ver 5 with each variable storing number at a position

--- 210050040_210050044_210050155_tile_looptile_loop.py
+++ 210050040_210050044_210050155_tile_looptile_loop.py
@@ -5,7 +5,7 @@
 
 
 from z3 import *
-import sys, re
+import sys
 file = sys.argv[1]
 
 # generate clause representing row/column "move" is incremented/decremented by one
@@ -31,8 +31,8 @@ all_states = []
 for t in range(T+1):
     one_state=[]
     for i in range(n*n):
-        one_number = Int(f"board_{t}_num_{i}_pos")
-        one_state.append(one_number)
+        one_pos = Int(f"board_{t}_pos_{i}_num")
+        one_state.append(one_pos)
     all_states.append(one_state)
 
 # variables defining the move done at every step
@@ -51,48 +51,45 @@ s = Solver()
 t = 0
 for i in range(n):
     for j in range(n):
-        s.add(all_states[t][matrix[i][j]-1]==i*n+j)
+        s.add(all_states[t][i*n+j]==matrix[i][j])
 
 # clause for win condition of game
 t = T
 win_state = []
-for required_num_at_idx in range(n*n):
-    s.add(all_states[t][required_num_at_idx]==required_num_at_idx)
-s.add(And(win_state))
+for pos in range(n*n):
+    s.add(all_states[t][pos]==pos+1)
 
 # clauses for changing state of board by applying a move
 for t in range(T):
-    for num in range(n*n):
-        for pos in range(n*n):
-            i = pos//n
-            j = pos%n
-            current_pos_clause = all_states[t][num] == pos
-            next_pos_same = all_states[t+1][num] == pos
-            move_clauses = [generate_move_clause(t,i,"1","1"),generate_move_clause(t,i,"1","0"),
-                            generate_move_clause(t,j,"0","1"),generate_move_clause(t,j,"0","0")]
-            s.add(Implies(current_pos_clause,Or([next_pos_same,]+move_clauses)))
-
-    # move_clauses = []
+    for pos in range(n*n):
+        i = pos//n
+        j = pos%n
+        move_clauses = [generate_move_clause(t,i,"1","1"),generate_move_clause(t,i,"1","0"),
+                        generate_move_clause(t,j,"0","1"),generate_move_clause(t,j,"0","0")]
+        s.add(Or([all_states[t][pos]==all_states[t+1][pos],]+move_clauses))
+for t in range(T):
     for (bool_inc,bool_row) in [("0","0"),("0","1"),("1","0"),("1","1")]:
         for i in range(n):
             move_clause = generate_move_clause(t, i, bool_row, bool_inc)
             is_inc = 1 if bool_inc == "1" else -1 # "1" - increment, "0" - decrement
             is_row = bool_row == "1" # "1" - row shift, "0" - column shift
             shift_clause = []
-            
             for j in range(n):
-                for num in range(n*n):
+                if is_row:
+                    current_position = i*n+j
+                    next_position = i*n+(j+is_inc)%n
+                else:
+                    current_position = j*n+i
+                    next_position = ((j+is_inc)%n)*n+i
+                shift_clause.append(all_states[t][current_position]==all_states[t+1][next_position])
+            for other_i in range(n):
+                if other_i==i:
+                    continue
+                for other_j in range(n):
                     if is_row:
-                        current_position = i*n+j
-                        next_position = i*n+(j+is_inc)%n
+                        shift_clause.append(all_states[t][other_i*n+other_j]==all_states[t+1][other_i*n+other_j])
                     else:
-                        current_position = j*n+i
-                        next_position = ((j+is_inc)%n)*n+i
-
-                    current_pos_clause = all_states[t][num] == current_position
-                    next_pos_clause = all_states[t+1][num] == next_position
-                    shift_clause.append(Implies(current_pos_clause,next_pos_clause))
-
+                        shift_clause.append(all_states[t][other_i+other_j*n]==all_states[t+1][other_i+other_j*n])
             s.add(Implies(move_clause,And(shift_clause)))
 
 # Output the moves
@@ -100,14 +97,14 @@ x = s.check()
 print(x)
 if x == sat:
     m = s.model()
-    moves = [{} for i in range(T)]
+    moves = [{} for t in range(T)]
     for d in m.decls():
         if "move" in d.name():
-            i = int(re.findall("move_[0-9]+",d.name())[0].strip("move_"))
-            moves[i][d.name()]=m[d]
-    for i in range(len(moves)):
-        type_of_move = [["u","d"],["l","r"]][1 if moves[i][f"move_{i}_row_or_col"] else 0][1 if moves[i][f"move_{i}_inc_or_dec"] else 0]
-        position_of_move = moves[i][f"move_{i}_pos"]
+            t = int(d.name().split("_")[1])
+            moves[t][d.name()]=m[d]
+    for t in range(len(moves)):
+        type_of_move = [["u","d"],["l","r"]][1 if moves[t][f"move_{t}_row_or_col"] else 0][1 if moves[t][f"move_{t}_inc_or_dec"] else 0]
+        position_of_move = moves[t][f"move_{t}_pos"]
         a = position_of_move<IntVal(n)
         if eval(str(a)):
             print(f"{position_of_move}{type_of_move}")
